395a887ee901743e78abbdf9e3fbfa0d165938d2
Ryan La Forge <rlaforge@vendasta.com>
2016-06-29 13:55:27 -0600

    Added first client test (not done yet), and jasmine-node typings. [#122207863]

--- src/client.spec.js
+++ src/client.spec.js
@@ -1,6 +1,22 @@
-describe('example test', function () {
-    it('should be true', function () {
-        expect('foo').toBe('foo');
+"use strict";
+var _this = this;
+/// <reference path="./protos/datariver.d.ts" />
+var client_1 = require("./client");
+var client_2 = require("./client");
+var protos_1 = require('./protos/protos');
+var datariver = protos_1.datariverProto.datariver.DataRiver;
+describe('Client tests', function () {
+    beforeEach(function () {
+        _this.client = new client_1.Client(client_2.Environment.TEST, "fake-token");
+    });
+    describe("getListing tests", function () {
+        it('Should call my callback method with the returned listing.', function () {
+            var fakeListing = new datariver.Listing();
+            datariver.getListing = jasmine.createSpy("getListing() spy").andReturn(fakeListing);
+            _this.client.getListing("fake listing id", function () {
+                console.log("I was called");
+            });
+        });
     });
 });
 //# sourceMappingURL=client.spec.js.map
--- src/client.spec.ts
+++ src/client.spec.ts
@@ -1,5 +1,20 @@
-describe('example test', function() {
-  it('should be true', function() {
-    expect('foo').toBe('foo');
+/// <reference path="./protos/datariver.d.ts" />
+import {Client} from "./client";
+import {Environment} from "./client";
+import {datariverProto} from './protos/protos'
+import {Listing} from './index';
+var datariver = datariverProto.datariver.DataRiver;
+describe('Client tests', () => {
+  beforeEach(() => {
+    this.client = new Client(Environment.TEST, "fake-token");
+  });
+  describe("getListing tests", () => {
+    it('Should call my callback method with the returned listing.', () => {
+      let fakeListing: datariver.Listing = new datariver.Listing();
+      datariver.getListing = jasmine.createSpy("getListing() spy").andReturn(fakeListing);
+      this.client.getListing("fake listing id", () => {
+        console.log("I was called");
+      })
+    });
   });
 });
