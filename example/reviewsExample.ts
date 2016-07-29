b8742becb48b6a8eaf28431c82c2d7874cb506a6
Ryan La Forge <rlaforge@vendasta.com>
2016-07-29 15:08:22 -0600

    add-support-for-list-reviews-by-listing-external-id: changing token back to my-example-token for tests

--- example/reviewsExample.ts
+++ example/reviewsExample.ts
@@ -1,5 +1,5 @@
 import {Client, Environment, Listing, Geo, Review, Timestamp, Empty} from "../src/index"
-const client = new Client(Environment.TEST, 'thisistestingonly');    // ask us for a token.
+const client = new Client(Environment.TEST, 'my-example-token');    // ask us for a token.
 
 
 // Create a listing object to put.
